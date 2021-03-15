4c4eea5f72a3748ffa70dcd5d239b1df7ff369b0
Jamie Warburton <jamie@hexdigital.com>
2021-03-15 08:06:09 +0000

    Initial commit from Sage 10

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    content: [
+      './app/**/*.php',
+      './resources/**/*.php',
+      './resources/**/*.vue',
+      './resources/**/*.js',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
